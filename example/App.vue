5e361eaecb064d06621941d760243acdf1869d74
Mikhail Kuznetcov <Mikhail.Kuznetcov@ing.com>
2019-01-19 21:24:08 +0100

    add clear func in example

--- example/App.vue
+++ example/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <vue-emoji @input="onInput" :value="myText" />
+<section>
+  <vue-emoji ref="emoji" width="400" @input="onInput" :value="myText" />
+  <br>
+  You've entered: {{readText}}<br>
+  <button @click="clearTextarea()">Clear</button>
+</section>
 </template>
 
 <script>
@@ -9,12 +14,17 @@ export default {
   name: 'app',
   data(){
     return {
-      myText: ''
+      myText: '',
+      readText: ''
     }
   },
   methods: {
+    clearTextarea(){
+      this.$refs.emoji.clear()
+    },  
     onInput(event){
      console.log(event.data)
+     this.readText = event.data
     }
   },
   components: {
